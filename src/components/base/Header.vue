7dbf653f7a5e65c496b0eff972cd3cd0f9aaede7
e <850525344@qq.com>
2018-03-16 11:13:22 +0800

    first commit

---
+++ src/components/base/Header.vue
@@ -0,0 +1,37 @@
+<template>
+	<header id="headerTab">
+		<p @click="changeParent">{{tabname}}</p>
+	</header>
+</template>
+
+<script>
+	export default {
+		props: ['tabname'],
+		data() {
+			return {
+
+			}
+		},
+		methods:{
+			changeParent(){
+				this.$emit("increment");
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	@import '../../../static/less/variable.less';
+	header {
+		height: .8rem;
+		text-align: center;
+		background: @theme_background;
+		color: @base_color;
+		font-size: .26rem;
+		line-height: .8rem;
+		width: 100%;
+		z-index: 10;
+		position: fixed;
+		top: 0;
+	}
+</style>
